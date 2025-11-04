c1028eac6a12845e40c9b89a812ac22660fd8d07
ahmedjhidri <167217614+ahmedjhidri@users.noreply.github.com>
2025-11-04 18:01:35 +0100

    Update main.jsx

--- tunisiecigares/src/main.jsx
+++ tunisiecigares/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { CartProvider } from './context/CartContext.jsx'; // ← AJOUTE
 import App from './App.jsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <CartProvider> {/* ← ENVELOPPE */}
+        <App />
+      </CartProvider>
     </HashRouter>
   </React.StrictMode>
 );
